a8a9410bade90a057b460062dae57873fede09d2
gauri <gauri.vashisht7@gmail.com>
2026-07-08 11:17:32 +0530

    create academic domain structure

--- app/database/models.py
+++ app/database/models.py
@@ -3,3 +3,11 @@
 from app.models.user import User
 
 __all__ = ["User"]
+
+
+from app.models.school import School
+from app.models.academic_year import AcademicYear
+from app.models.classroom import Classroom
+
+from app.models.teacher_profile import TeacherProfile
+from app.models.student_profile import StudentProfile
